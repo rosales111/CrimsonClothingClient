c6563931f88fc4bace033a18cdcec4fcaeeb0352
Orion Rosales <osrosalesalfaro@crimson.ua.edu>
2023-04-24 14:35:13 -0500

    cart.js in

---
+++ resources/scripts/cart.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    // Get all the item cards
+    const itemCards = $('.card.item');
+  
+    // Initialize the total price to 0
+    let totalPrice = 0;
+  
+    // Loop through each item card and add event listener to the remove button
+    itemCards.each(function() {
+      const removeButton = $(this).find('.remove-button');
+      const priceText = $(this).find('.item-price p').text();
+      const price = parseFloat(priceText.substring(1));
+  
+      removeButton.click(function() {
+        // Remove the item card from the cart
+        $(this).closest('.card').remove();
+  
+        // Subtract the price of the item from the total price
+        totalPrice -= price;
+        $('.total-price p').text('$' + totalPrice.toFixed(2));
+      });
+  
+      // Add the price of the item to the total price
+      totalPrice += price;
+    });
+  
+    // Update the total price in the cart
+    $('.total-price p').text('$' + totalPrice.toFixed(2));
+  });
+  
