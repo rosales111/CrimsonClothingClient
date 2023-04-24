ca27837f87ff09961452a7bea964efecb9e35ac4
Orion Rosales <osrosalesalfaro@crimson.ua.edu>
2023-04-24 12:53:23 -0500

    cards in main

--- resources/scripts/createAccount.js
+++ resources/scripts/createAccount.js
@@ -1,30 +1,43 @@
 // create-account.js
 
-const form = document.querySelector('#create-account-form');
+const url = "https://localhost:7026/api/Users";
+const form = document.querySelector("#register-form");
 
-form.addEventListener('submit', async (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
-  const email = document.querySelector('#email').value;
-  const password = document.querySelector('#password').value;
-  const confirmPassword = document.querySelector('#confirm-password').value;
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+  const fullname =
+    document.querySelector("#firstName").value +
+    " " +
+    document.querySelector("#lastName").value;
+  const confirmPassword = document.querySelector("#confirm-password").value;
 
   if (password !== confirmPassword) {
     // display an error message if the passwords do not match
     return;
   }
 
+  let newUser = {
+    fullname: fullname,
+    email: email,
+    password: password,
+    role: 1,
+  };
+
   try {
-    const response = await fetch('/api/users', {
-      method: 'POST',
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(newUser),
       headers: {
-        'Content-Type': 'application/json'
+        "Content-type": "application/json; charset=UTF-8",
       },
-      body: JSON.stringify({ email, password })
     });
 
     if (response.ok) {
       // handle successful user creation
+      form.reset();
     } else {
       // handle user creation error
     }
--- resources/scripts/login.js
+++ resources/scripts/login.js
@@ -1,44 +1,41 @@
-const form = document.getElementById('login-form');
+const form = document.getElementById("login-form");
 
-form.addEventListener('submit', async (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault(); // prevent the form from submitting normally
 
-  const email = document.getElementById('email').value;
-  const passwordInput = document.getElementById('password');
+  const email = document.getElementById("email").value;
+  const passwordInput = document.getElementById("password").value;
   const password = passwordInput.value;
 
   // validate email format using regex
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    // handle invalid email error
-    alert('Please enter a valid email address');
-    return;
-  }
+  // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // if (!emailRegex.test(email)) {
+  //   // handle invalid email error
+  //   alert("Please enter a valid email address");
+  //   return;
+  // }
 
-  if (password.length < 8) {
-    alert('Password must be at least 8 characters long.');
-    passwordInput.focus();
-    return;
-  }
+  // if (password.length < 8) {
+  //   alert("Password must be at least 8 characters long.");
+  //   passwordInput.focus();
+  //   return;
+  // }
 
-  if (!/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])/.test(password)) {
-    alert('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.');
-    passwordInput.focus();
-    return;
-  }
+  // if (!/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])/.test(password)) {
+  //   alert(
+  //     "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+  //   );
+  //   passwordInput.focus();
+  //   return;
+  // }
 
-  const response = await fetch('/api/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ email, password })
-  });
-
-  if (response.ok) {
-    // handle successful login
-    window.location.href = '/home-page.html';
-  } else {
-    // handle login error
-  }
+  let userUrl = `https://localhost:7026/api/Users/byemail/${email}`;
+  console.log(`https://localhost:7026/api/Users/byemail/${email}`);
+  fetch(userUrl)
+    .then((response) => response.json())
+    .then((user) => {
+      console.log(user);
+    })
+    .catch((error) => console.error(error));
+  form.reset();
 });
