31060c71bac2d3780548f4f334dba8c988b28858
Orion Rosales <osrosalesalfaro@crimson.ua.edu>
2023-04-26 18:30:55 -0500

    manager-navigations

--- resources/scripts/manager-reports.js
+++ resources/scripts/manager-reports.js
@@ -1,16 +1,11 @@
-// Fetch data from server
 fetch('/api/sales')
   .then(response => response.json())
   .then(data => {
-    // Manipulate data to create chart
     const chartData = processData(data);
-
-    // Render chart using chart library
     renderChart(chartData);
   })
   .catch(error => console.error(error));
 
-// Process data to create chart data
 function processData(data) {
   const chartData = {
     labels: [],
@@ -21,7 +16,6 @@ function processData(data) {
     }]
   };
 
-  // Loop through data and group sales by category
   const salesByCategory = {};
   data.forEach(sale => {
     if (!salesByCategory[sale.category]) {
@@ -30,7 +24,6 @@ function processData(data) {
     salesByCategory[sale.category] += sale.amount;
   });
 
-  // Add data to chartData
   for (const category in salesByCategory) {
     chartData.labels.push(category);
     chartData.datasets[0].data.push(salesByCategory[category]);
@@ -40,7 +33,6 @@ function processData(data) {
   return chartData;
 }
 
-// Render chart using chart library
 function renderChart(data) {
   const ctx = document.getElementById('chart').getContext('2d');
   const chart = new Chart(ctx, {
@@ -58,7 +50,6 @@ function renderChart(data) {
   });
 }
 
-// Generate random color for chart background
 function getRandomColor() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
